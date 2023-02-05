feef96b86c7b1ed6f7798fc0e808028c2fb00cd5
seyeong <yeongpsy@gmail.com>
2023-02-05 18:03:49 +0900

    vue_todo_1

--- Vuejs/vue30컴포넌트실습/comprj01/src/App.vue
+++ Vuejs/vue30컴포넌트실습/comprj01/src/App.vue
@@ -10,6 +10,7 @@
       <router-link to="/3701">3701</router-link> |
       <router-link to="/imageSlider">imageSlider</router-link> |
       <router-link to="/4301">4301</router-link> |
+      <router-link to="/todo">todo</router-link> |
     </nav>
     <router-view />
   </div>
